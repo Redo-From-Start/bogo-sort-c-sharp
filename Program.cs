526d3056cc788a9a5126a8873ef9ca99eff56d0d
Redo From Start <55228370+Redo-From-Start@users.noreply.github.com>
2020-01-07 21:12:08 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace bogosort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] old = { 1, 6, 42, 6, 43, 234, 68, 2, 7 };
+            int[] a = bogo(old);
+
+            foreach (int i in a)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        public static int[] bogo(int[] old)
+        {
+            int[] ne = old;
+            bool done = false;
+
+            Random rng = new Random();
+
+            while (!done)
+            {
+                int n = ne.Length;
+                while (n > 1)
+                {
+                    int k = rng.Next(n--);
+                    int temp = ne[n];
+                    ne[n] = ne[k];
+                    ne[k] = temp;
+                }
+
+                int p = 0;
+
+                done = true;
+
+                foreach (int i in ne)
+                {
+                    if (p > i)
+                    {
+                        done = false;
+                        break;
+                    }
+
+                    p = i;
+                }
+            }
+
+            return ne;
+        }
+    }
+}
